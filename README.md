88ecaa88982e70080b1686acc2c737a65d4d181f
Sonic <sonic.wong@deliveryrepublic.com>
2017-05-26 18:03:52 +0800

    Init release.

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# ionic-modal-transition-pack
-Transition class for Ionic v3+ (Animation based on animate.css)
+## Transition class for Ionic v3+ (Animation based on animate.css) ##
+https://github.com/sonicwong/ionic-modal-transition-pack
+
+# How to use
+1) Download the "ionic-modal-transition-pack.ts" and place it under "src/classes".
+
+2) Add below code to "app.module.ts":
+<pre>
+import { Config } from 'ionic-angular';
+import {
+    ModalEnterDirect, ModalLeaveDirect
+    ,ModalEnterFadeIn, ModalLeaveFadeOut
+    ,ModalEnterZoomIn, ModalLeaveZoomIn
+    ,ModalEnterZoomOut, ModalLeaveZoomOut
+} from '../classes/ionic-modal-transition-pack';
+
+export class AppModule {
+    constructor(public config: Config) {
+        this.setCustomTransitions();
+    }
+    
+    private setCustomTransitions() {
+        this.config.setTransition('ModalEnterDirect', ModalEnterDirect);
+        this.config.setTransition('ModalLeaveDirect', ModalLeaveDirect);
+        
+        this.config.setTransition('ModalEnterFadeIn', ModalEnterFadeIn);
+        this.config.setTransition('ModalLeaveFadeOut', ModalLeaveFadeOut);
+        
+        this.config.setTransition('ModalEnterZoomIn', ModalEnterZoomIn);
+        this.config.setTransition('ModalLeaveZoomIn', ModalLeaveZoomIn);
+        
+        this.config.setTransition('ModalEnterZoomOut', ModalEnterZoomOut);
+        this.config.setTransition('ModalLeaveZoomOut', ModalLeaveZoomOut);
+    }
+}
+</pre>
+
+3) Add below code to "yourView.ts":
+<pre>
+public presentYourModal() {
+    const yourModal = this.modalController.create(YourModalComponent, { userId: 46 }, {
+        showBackdrop: false,
+        enableBackdropDismiss: false,
+        enterAnimation: 'modal-scale-up-enter',
+        leaveAnimation: 'modal-scale-up-leave'
+    });
+    yourModal.present();
+}
+
+this.modal = this.modalCtrl.create(ModalContentPage, {someVar: 'someValue'}, {
+    showBackdrop: false,   // <-- this is not support yet
+    enableBackdropDismiss: false,
+    enterAnimation: 'ModalEnterZoomOut',
+    leaveAnimation: 'ModalLeaveZoomOut'                
+});
+</pre>
+
+# BUGS
+"showBackdrop" option not support yet, or backdrop will always show.
+
+# Copyright
+sw2hk.com
---
+++ ionic-modal-transition-pack.ts
@@ -0,0 +1,158 @@
+// code base on: https://github.com/driftyco/ionic/blob/master/src/components/modal/modal-transitions.ts
+// animation base on: https://github.com/daneden/animate.css/blob/master/animate.css
+import { Animation, PageTransition } from 'ionic-angular';
+
+
+/* ==================================================
+Direct
+================================================== */
+export class ModalEnterDirect extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.enteringView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.beforeStyles({ 'opacity': 1 });
+        backdrop.beforeStyles({ 'opacity': 0.4 });
+        this
+            .element(this.enteringView.pageRef())
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+export class ModalLeaveDirect extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.leavingView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.beforeStyles({ 'opacity': 0 });
+        backdrop.beforeStyles({ 'opacity': 0 });
+        this
+            .element(this.leavingView.pageRef())
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+
+/* ==================================================
+Fade
+================================================== */
+export class ModalEnterFadeIn extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.enteringView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.beforeStyles({ 'opacity': 1 });
+        wrapper.fromTo('opacity', 0, 1);
+        backdrop.fromTo('opacity', 0.01, 0.4);
+        this
+            .element(this.enteringView.pageRef())
+            .easing('ease-in')
+            .duration(400)
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+export class ModalLeaveFadeOut extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.leavingView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.fromTo('opacity', 1, 0);
+        backdrop.fromTo('opacity', 0.4, 0.0);
+        this
+            .element(this.leavingView.pageRef())
+            .easing('ease-out')
+            .duration(250)
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+
+/* ==================================================
+ZoomIn
+================================================== */
+export class ModalEnterZoomIn extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.enteringView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.beforeStyles({ 'opacity': 1 });
+        wrapper.fromTo('transform', 'scale3d(.3, .3, .3)', 'scale3d(1, 1, 1)');
+        backdrop.fromTo('opacity', 0.01, 0.4);
+        this
+            .element(this.enteringView.pageRef())
+            .easing('ease-in')
+            .duration(400)
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+export class ModalLeaveZoomIn extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.leavingView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.fromTo('transform', 'scale3d(1, 1, 1)', 'scale3d(1.3, 1.3, 1.3)');
+        wrapper.fromTo('opacity', 1, 0);
+        backdrop.fromTo('opacity', 0.4, 0.0);
+        this
+            .element(this.leavingView.pageRef())
+            .easing('ease-out')
+            .duration(250)
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+
+/* ==================================================
+ZoomOut
+================================================== */
+export class ModalEnterZoomOut extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.enteringView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.beforeStyles({ 'opacity': 1 });
+        wrapper.fromTo('transform', 'scale3d(1.3, 1.3, 1.3)', 'scale3d(1, 1, 1)');
+        backdrop.fromTo('opacity', 0.01, 0.4);
+        this
+            .element(this.enteringView.pageRef())
+            .easing('ease-in')
+            .duration(400)
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+export class ModalLeaveZoomOut extends PageTransition {
+    public init() {
+        super.init();
+        const ele: HTMLElement = this.leavingView.pageRef().nativeElement;
+        const wrapper = new Animation(this.plt, ele.querySelector('.modal-wrapper'));
+        const backdrop = new Animation(this.plt, ele.querySelector('ion-backdrop'));
+        wrapper.fromTo('transform', 'scale3d(1, 1, 1)', 'scale3d(.3, .3, .3)');
+        wrapper.fromTo('opacity', 1, 0);
+        backdrop.fromTo('opacity', 0.4, 0.0);
+        this
+            .element(this.leavingView.pageRef())
+            .easing('ease-out')
+            .duration(250)
+            .add(backdrop)
+            .add(wrapper);
+    }
+}
+
+
